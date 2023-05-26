fd0ca853f75d3fef8fecb91316ca41f83057c688
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-05-26 10:04:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,36 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
+std::string ComposeWord(std::ifstream& in) {
+    std::string out;
+    do {
+        char lt = in.get();
+        if ((lt > 64 && lt < 91) || (lt > 96 && lt < 123)) {
+            if (lt > 64 && lt < 91) {
+                lt += 32;
+            }
+            out += lt;
+        } else {
+            break;
+        }
+    } while (1);
+    return out;
+}
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST <std::string> resultTree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Unfortuantely can't open file " << std::endl;
+        return BST<std::string>();
+    }
+    while (!file.eof()) {
+        std::string composed = ComposeWord(file);
+        if (!composed.empty()) {
+            resultTree.addNode(composed);
+        } else {
+            continue;
+        }
+    }
+    file.close();
+    return resultTree;
 }
